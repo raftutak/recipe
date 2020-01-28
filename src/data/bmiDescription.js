31aaefd727450457c74e17ed938676768ad6195c
Kurylo <extern.daniel.kurylo@vw-poznan.pl>
2020-01-28 18:34:29 +0100

    recent changes updated

--- src/data/bmiDescription.js
+++ src/data/bmiDescription.js
@@ -1,7 +1,20 @@
 export const bmiDescription = [
     {
+        id: 1,
         heading: "Czym jest BMI?",
         paragraph:
         "BMI jest jednym z ważnych wskaźniów określających nasz stan fizyczny, ale niestety nie wystarczającym. Bardzo ważnym uzupełnieniem BMI jest wskaźnik ilości tłuszczu brzusznego - zbyt duży może oznaczać niebezpieczną otyłość brzuszną i to nawet przy prawidłowym BMI! Ponadto, paradoksalnie, badania naukowe wskazują, że osoby z lekką nadwagą zwykle są zdrowsze i żyją dłużej od osób z tzw. \"prawidłową wagą\". Pojawiają się nawet głosy, że ustalony arbitralnie przez WHO próg nadwagi (25) jest zbyt niski."
+    },
+    {
+        id: 2,
+        heading: "Kalkulator BMI",
+        paragraph:
+        "Kalkulator BMI (Body Mass Index) daje każdemu możliwość szybkiego i wygodego obliczenia własnego wskaźnika masy ciała. BMI obliczamy dzieląc masę ciała (w kilogramach) przez wzrost do kwadratu (w metrach). Wskaźnik ten wykorzystywany jest przede wszystkim do oceny ryzyka pojawienia się groźnych chorób: miażdżycy, choroby niedokrwiennej serca, udaru mózgu, czy nawet nowotworów. Większość tych chorób jest związana z otyłością i dlatego kalkulator BMI to tak przydatne narzędzie."
+    },
+    {
+        id: 3,
+        heading: "Warto pamiętać.",
+        paragraph:
+        "Kalkulator BMI obrazuje przybliżoną zawartość tłuszczu w organiźmie. W przypadku niektórych osób wskaźnik BMI może sugerować błędne wnioski. Osoby aktywne fizycznie, uprawiający sport, mogą posiadać zawyżoną wagę związaną z tkanką mięśniową a nie z ilością tłuszczu w organiźmie. Ponadto nie zaleca się stosowania wskaźnika BMI do oznaczania wagi ciała dla dzieci do ok. 14 roku życia oraz dla kobiet w ciąży."
     }
 ];
--- src/data/slider.js
+++ src/data/slider.js
@@ -14,5 +14,13 @@ export const slider = [
       'Kalkulator BMI (Body Mass Index) daje każdemu możliwość szybkiego i wygodego obliczenia własnego wskaźnika masy ciała. BMI obliczamy dzieląc masę ciała (w kilogramach) przez wzrost do kwadratu (w metrach). Wskaźnik ten wykorzystywany jest przede wszystkim do oceny ryzyka pojawienia się groźnych chorób oraz oceny stanu zdrowia.',
     button: 'Przejdź do Kalkulatora BMI',
     url: 'routes.calculatorBMI'
+  },
+  {
+    id: 3,
+    heading: 'Przelicznik Miar Kuchennych',
+    paragraph:
+      'Nie wiesz jak przeliczyć gramy na szklanki? Łyżki na gramy? Skorzystaj z naszej tabeli przeliczników miar kuchennych poszczególnych produktów żywnościowych. Pamiętaj, że szklanka to 250 cm³, łyżka stołowa wynosi 15 cm³, a jednej łyżce stołowej mieszczą się średnio trzy małe łyżeczki.',
+    button: 'Przejdź do Przelicznika',
+    url: 'routes.calculatorBMI'
   }
 ];
