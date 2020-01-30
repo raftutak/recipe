93f6878594e3f92344ad4a144b2573e3f322502a
raftutak <raftutak@gmail.com>
2020-01-30 02:15:10 +0100

    SingleRecipe development

--- src/components/organisms/SingleRecipeSection.js
+++ src/components/organisms/SingleRecipeSection.js
@@ -11,28 +11,35 @@ import history from '../../utils/history';
 
 class SingleRecipeSection extends React.Component {
   state = {
-    singleRecipe_result: undefined
+    singleRecipe: {
+      isLoading: true,
+      result: undefined
+    }
   };
 
-  handleReadRecipe = async () => {
-    await this.setState({
-      singleRecipe_isLoading: true,
-      singleRecipe_result: undefined
+  handleSingleRecipe = async () => {
+    this.setState({
+      singleRecipe: {
+        isLoading: true,
+        result: undefined
+      }
     });
 
     const id = this.props.id;
-    const url = `https://recipe-search.projektstudencki.pl/recipe/searchRecipes/?id=${id}`;
-
+    const url = `https://recipe-search.projektstudencki.pl/recipe/SearchRecipeModel/?id=${id}`;
     const response = await axios(url);
-    const singleRecipe_result = await response.data.recipe;
+    const result = await response.data.recipe;
 
-    this.setState({ singleRecipe_isLoading: false, singleRecipe_result });
-
-    console.log(this.state.singleRecipe_result);
+    this.setState({
+      singleRecipe: {
+        isLoading: false,
+        result
+      }
+    });
   };
 
   componentDidMount() {
-    this.handleReadRecipe();
+    this.handleSingleRecipe();
   }
 
   componentDidUpdate(prevProps) {
@@ -44,26 +51,26 @@ class SingleRecipeSection extends React.Component {
   render() {
     // const { id } = this.props;
 
-    return this.state.singleRecipe_result ? (
+    return this.state.singleRecipe && this.state.singleRecipe.result ? (
       <>
         <StyledSingleRecipeContainer fluid>
           <Container>
             <Row className="my-4">
               <Col style={{ textAlign: 'center' }}>
-                <h2 className="mb-3">{this.state.singleRecipe_result.title}</h2>
+                <h2 className="mb-3">{this.state.singleRecipe.result.title}</h2>
                 <p className="mb-1">
                   Przepis pochodzi z serwisu:{' '}
-                  <strong>{this.state.singleRecipe_result.blog}</strong>
+                  <strong>{this.state.singleRecipe.result.blog}</strong>
                 </p>
                 <p className="mb-0 text-break">
-                  {this.state.singleRecipe_result.source_Url}
+                  {this.state.singleRecipe.result.source_Url}
                 </p>
               </Col>
             </Row>
             <Row className="mb-4">
               <Col xs={12} md={6} lg={6}>
                 <StyledImageBackground
-                  background={this.state.singleRecipe_result.image_Url}
+                  background={this.state.singleRecipe.result.image_Url}
                 />
               </Col>
               <Col xs={12} md={6} lg={6}>
@@ -71,7 +78,7 @@ class SingleRecipeSection extends React.Component {
                   <ListGroup.Item>
                     <strong>Składniki</strong>
                   </ListGroup.Item>
-                  {this.state.singleRecipe_result.ingredients.map(item => {
+                  {this.state.singleRecipe.result.ingredients.map(item => {
                     return <ListGroup.Item>{item}</ListGroup.Item>;
                   })}
                 </ListGroup>
@@ -79,7 +86,7 @@ class SingleRecipeSection extends React.Component {
             </Row>
             <Row className="mb-4">
               <Col xs={12} md={12} lg={12}>
-                {this.state.singleRecipe_result.description}
+                {this.state.singleRecipe.result.description}
               </Col>
             </Row>
             <div>
