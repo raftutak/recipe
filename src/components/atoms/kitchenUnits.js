f911098c98f2fc12b1699a6919b9d83470e0f61e
Kurylo <extern.daniel.kurylo@vw-poznan.pl>
2020-01-30 22:58:15 +0100

    converter update #2

--- src/components/atoms/kitchenUnits.js
+++ src/components/atoms/kitchenUnits.js
@@ -2,13 +2,17 @@ export const kitchenUnits = (ml) => {
     let unit, value
 
     if (ml <= 5) {
-        unit = 'łyżeczka', value = ml
+        unit = 'łyżeczka'
+        value = ml
     } else if ( ml > 5 && ml <= 15 ) {
-        unit = 'łyżka', value = ml
+        unit = 'łyżka'
+        value = ml
     } else if ( ml > 15 && ml <= 250 ) {
-        unit = 'szklanka', value = ml 
+        unit = 'szklanka'
+        value = ml 
     } else {
-        unit = 'ml', value = ml
+        unit = 'ml'
+        value = ml
     }
 
     return {unit: unit, value: value}
--- src/components/molecules/Converter.js
+++ src/components/molecules/Converter.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Form, Col } from 'react-bootstrap';
+import { Panel } from 'react-bootstrap';
 
 class Converter extends React.Component {
 
@@ -13,27 +14,50 @@ class Converter extends React.Component {
 
     }
 
-    handleOnChange = (property, value) => {
-      this.setState({[`${property.category}`]: {...this.state[`${property.category}`], [`${property.unit}`]: value}})
+    handleVolumeChange = (newVolume) => {
+      this.setState({
+        volume: newVolume
+      });
+
+        return( 
+        <>
+        {this.state.volume}
+        </>
+        );
+
     }
 
-    kitchenUnits = (ml) => {
-      let unit, value
-  
-      if (ml <= 5) {
-          unit = 'łyżeczka', value = ml
-      } else if ( ml > 5 && ml <= 15 ) {
-          unit = 'łyżka', value = ml
-      } else if ( ml > 15 && ml <= 250 ) {
-          unit = 'szklanka', value = ml 
-      } else {
-          unit = 'ml', value = ml
-      }
-  
-      return {unit: unit, value: value}
-  
-  }
-  
+    handleWeightChange = (newWeight) => {
+      this.setState({
+        weight: newWeight
+      });
+    }
+
+    // handleOnChange = (property, value) => {
+    //   this.setState({[`${property.category}`]: {...this.state[`${property.category}`], [`${property.unit}`]: value}})
+    // }
+
+// kitchenUnits = (ml) => {
+//     let unit, value
+
+//     if (ml <= 5) {
+//         unit = 'łyżeczka'
+//         value = ml
+//     } else if ( ml > 5 && ml <= 15 ) {
+//         unit = 'łyżka'
+//         value = ml
+//     } else if ( ml > 15 && ml <= 250 ) {
+//         unit = 'szklanka'
+//         value = ml 
+//     } else {
+//         unit = 'ml'
+//         value = ml
+//     }
+
+//     return {unit: unit, value: value}
+
+// }
+
 
   render() {
     return (
@@ -42,6 +66,29 @@ class Converter extends React.Component {
             <h3>
               Przelicznik Miar Kuchennych
             </h3>
+        </Row>
+        <Row>
+          <Col xs={12} md={6} lg={6}>
+            <Form onSubmit={this.SubmitButton}>
+
+              <p className="mb-2">
+                Wprowadź ilość:
+              </p>
+
+              <input
+                type="number"
+                //className="field"
+                name="volume"
+                required
+                value={this.state.volume}
+               // onBlur={this.blur}
+                onChange={this.handleVolumeChange}
+              />
+
+
+
+            </Form>
+          </Col>
         </Row>
 
       </Container>
